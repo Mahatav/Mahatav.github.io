587371e986d1d0a1c8f42f2b302f4c44f519f521
Mahatav Arora <mahatavarora@gmail.com>
2026-06-20 20:55:51 -0700

    seo maxing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://mahatav.github.io",
   output: "static",
-  integrations: [sitemap()],
   server: {
     host: true,
     port: 4321,
